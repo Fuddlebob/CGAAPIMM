9c90ee52ee963da3f24b7678ca4181601eba7fd0
Fuddlebob <thomas@Thomas-PC.localdomain>
2020-04-21 06:39:44 +1000

    Extended max size decrease to zoom transform. TODO: Move max_size to config file for easy modification

--- transform/zoom.py
+++ transform/zoom.py
@@ -11,7 +11,7 @@ class zoomTransform(abstractTransform.abstractTransformClass):
 	
 	def description():
 		#return a brief description of what the transform does
-		return "Zooms in on a random square of the image and resizes it to a 2048x2048 square."
+		return "Zooms in on a random square of the image and resizes it to a 1024x1024 pixel square."
 		
 	def transform(image):
 		#take in an openCV image and transform it, returning the new image
@@ -20,4 +20,4 @@ class zoomTransform(abstractTransform.abstractTransformClass):
 		size = max(64, int(random.uniform(0.05, 0.4) * ma))
 		w2, h2 = randomPoint(w - size, h - size)
 		zoom = image[h2:h2+size, w2:w2+size]
-		return cv2.resize(zoom, (2048, 2048))
+		return cv2.resize(zoom, (1024, 1024))
